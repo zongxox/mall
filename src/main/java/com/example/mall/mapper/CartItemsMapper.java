3ed080e191ba101e329e7f8395b4134c32135c8e
zongxox <s520yi2930@gmail.com>
2025-10-29 11:20:13 +0800

    移動點擊事件位子

--- src/main/java/com/example/mall/mapper/CartItemsMapper.java
+++ src/main/java/com/example/mall/mapper/CartItemsMapper.java
@@ -7,13 +7,8 @@ import org.apache.ibatis.annotations.Mapper;
 public interface CartItemsMapper {
     //加入購物車
     int insertCart(CartItems cartItems);
-
-
-    //List<Cart> selectProductIdsQuantityInCartByUserId(Integer user_id);//透過userid顯示購物車商品
+    //透過userid顯示購物車商品
     //查詢該使用者購物車中所有商品規格的詳細資料（例如圖片、顏色、尺寸、價格、數量）
-    //List<CartItemDetailVO> selectCartDetailsByUserId(Integer userId);
-
-    //int deleteCart(Integer userId,Integer cartItemId);//刪除購物車商品
-
-   //int updateQuantityByCartItemId(Integer id,Integer quantity);//更新購物車數量
+    //刪除購物車商品
+    //更新購物車數量
 }
